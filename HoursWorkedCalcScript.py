6c83e4b263973f1049b8208a24ef95b60836ecfb
qazga <qazgal1@gmail.com>
2024-06-19 19:14:08 +0300

    finished Proj for makom balev calc for now added april and organised

--- HoursWorkedCalcScript.py
+++ HoursWorkedCalcScript.py
@@ -22,12 +22,11 @@ def getSalaryPrice(day):
     return 45
 
 
-
 TotalHoursWorked = 0
 TotalPay = 0
 totalReport = []
 adress = "Pardes Meshutaf St 5, Ra\u0027anana, 43355, Israel"
-f = open('2024_MAY.json', encoding="utf8")
+f = open('2024_APRIL.json', encoding="utf8")
 data = json.load(f)["timelineObjects"]
 values = [d['placeVisit'] for d in data if 'placeVisit' in d]
 workInstanses = []
@@ -37,7 +36,6 @@ for place in values:
             workInstanses.append(place["duration"])
     except:
         pass
-print(len(workInstanses))
 for times in workInstanses:
     startTime = parser.parse(times["startTimestamp"])
     endTime = parser.parse(times["endTimestamp"])
@@ -46,17 +44,19 @@ for times in workInstanses:
     end_obj_israel = endTime.astimezone(israel)
     durationTimeWorked = (end_obj_israel - start_obj_israel).total_seconds() / 3600
     TotalHoursWorked = TotalHoursWorked + durationTimeWorked
-    TotalDayPay = getSalaryPrice(start_obj_israel)*durationTimeWorked
-    TotalPay = TotalPay+TotalDayPay
-    totalReport.append({"startTime": start_obj_israel, "endTime": end_obj_israel, "Duration": durationTimeWorked,"pay": getSalaryPrice(start_obj_israel),"totalPay": TotalDayPay})
+    TotalDayPay = getSalaryPrice(start_obj_israel) * durationTimeWorked
+    TotalPay = TotalPay + TotalDayPay
+    totalReport.append({"startTime": start_obj_israel, "endTime": end_obj_israel, "Duration": durationTimeWorked,
+                        "pay": getSalaryPrice(start_obj_israel), "totalPay": TotalDayPay})
 
 for dayOfWork in totalReport:
-    print("------------------------------------------------------")
     print(" day: ", dayOfWork["startTime"].day, "\n",
           f"start: {dayOfWork["startTime"].hour}:{dayOfWork["startTime"].minute}\n",
           f"end: {dayOfWork["endTime"].hour}:{dayOfWork["endTime"].minute}\n",
           f"HoursWorked: {dayOfWork["Duration"]}",
           f"hourly: {dayOfWork["pay"]}",
           f"Total: {dayOfWork["totalPay"]}")
+    print("-----------------------------------------------------------------------")
+
 print(TotalHoursWorked)
 print(TotalPay)
